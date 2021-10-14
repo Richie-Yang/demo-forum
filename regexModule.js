c936b12043644976dd378e8d11dc2fdc8552c645
richieyang8813@gmail.com <richieyang8813@gmail.com>
2021-10-14 12:18:37 +0800

    refactor: search function

--- regexModule.js
+++ regexModule.js
@@ -1,21 +1,21 @@
-function commentParsing(comments, comment, index) {
+function commentParsing(item, index, array) {
   const commentRegex = /(.{1,20})/
-  const commentResult = commentRegex.exec(comment.comment)[1]
-  return comments[index]['brief_comment'] = `${commentResult}......`
+  const commentResult = commentRegex.exec(item.comment)[1]
+  return array[index]['brief_comment'] = `${commentResult}......`
 }
 
-function timeParsing(comments, comment, index) {
+function timeParsing(item, index, array) {
   const timeRegex = /(\w{1,5}\s\w{1,5}\s\d{2}\s\d{4}\s\d{2}\:\d{2}\:\d{2})/
-  const timeResult = timeRegex.exec(comment.created_at)[1]
-  return comments[index]['brief_updated_at'] = timeResult
+  const timeResult = timeRegex.exec(item.created_at)[1]
+  return array[index]['brief_updated_at'] = timeResult
 }
 
-function parsingController(comments) {
-  comments.forEach((comment, index, array) => {
-    commentParsing(array, comment, index)
-    timeParsing(array, comment, index)
+function regexController(array) {
+  array.forEach((item, index) => {
+    commentParsing(item, index, array)
+    timeParsing(item, index, array)
   })
-  return comments
+  return array
 }
 
-module.exports = parsingController
+module.exports = regexController
